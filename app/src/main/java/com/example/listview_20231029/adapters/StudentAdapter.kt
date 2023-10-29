e4dcbe635b23ac2ad6d3b5d05561c73d6b8b8b7c
Torku7 <saturn9908>
2023-10-29 11:28:19 +0900

    학생 데이터 + xml을 조합해서 뿌려줄 클래스 StudentAdapter 생성 -ArrayAdapter 상속 =>기본생성자 지원 안함 -어댑터 클래스가 생성자에서 변수 받아오게 -받아오는 변수를 부모 클래스에 전달

---
+++ app/src/main/java/com/example/listview_20231029/adapters/StudentAdapter.kt
@@ -0,0 +1,12 @@
+package com.example.listview_20231029.adapters
+
+import android.content.Context
+import android.widget.ArrayAdapter
+import com.example.listview_20231029.datas.StudentData
+
+class StudentAdapter(
+    mContext: Context,
+    resId: Int,
+    mList: ArrayList<StudentData>
+    ) : ArrayAdapter<StudentData>(mContext,resId,mList) {
+}
