d2c13e2f391be29e5786d51e4406751d09781a56
Torku7 <saturn9908>
2023-10-29 13:39:14 +0900

    정보 입력

--- app/src/main/java/com/example/listview_20231029/adapters/StudentAdapter.kt
+++ app/src/main/java/com/example/listview_20231029/adapters/StudentAdapter.kt
@@ -5,13 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.TextView
 import com.example.listview_20231029.R
 import com.example.listview_20231029.datas.StudentData
 
 class StudentAdapter(
-    mContext: Context,
-    resId: Int,
-    mList: ArrayList<StudentData>
+    val mContext: Context,
+    val resId: Int,
+    val mList: ArrayList<StudentData>
     ) : ArrayAdapter<StudentData>(mContext,resId,mList) {
 
         val inf = LayoutInflater.from(mContext)
@@ -25,6 +26,17 @@ class StudentAdapter(
         }
 
         val row = tempRow!!
+
+        val stdData = mList[position]
+
+        val txtname = row.findViewById<TextView>(R.id.txtName)
+        val txtPhoneNum = row.findViewById<TextView>(R.id.txtPhoneNum)
+        val txtAge = row.findViewById<TextView>(R.id.txtAge)
+
+        txtname.text = stdData.name
+        txtPhoneNum.text = stdData.phoneNum
+
+        txtAge.text = "(${stdData.getKoreanAge(2023)})세"
 
         return row
 
--- app/src/main/java/com/example/listview_20231029/datas/StudentData.kt
+++ app/src/main/java/com/example/listview_20231029/datas/StudentData.kt
@@ -1,5 +1,16 @@
 package com.example.listview_20231029.datas
 
-class StudentData(name: String, birthYear:Int, phoneNum: String) {
+import android.util.Log
 
+class StudentData(val name: String,val birthYear:Int,val phoneNum: String) {
+
+    constructor(name: String, birthYear: Int): this(name, birthYear,"폰번 모름")
+
+    fun getKoreanAge(year : Int) : Int{
+        return year - this.birthYear +1
+    }
+
+    fun getSimplePhoneNum(){
+        Log.d("학생데이터",this.phoneNum.replace("-",""))
+    }
 }
