c9efc04956446cb050f6f43e8f9ee16509974175
Torku7 <saturn9908>
2023-10-29 11:35:34 +0900

    어댑터 클래서에서, 리스트 아이템xml을 끌어오는 작업

--- app/src/main/java/com/example/listview_20231029/adapters/StudentAdapter.kt
+++ app/src/main/java/com/example/listview_20231029/adapters/StudentAdapter.kt
@@ -1,7 +1,11 @@
 package com.example.listview_20231029.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.example.listview_20231029.R
 import com.example.listview_20231029.datas.StudentData
 
 class StudentAdapter(
@@ -9,4 +13,21 @@ class StudentAdapter(
     resId: Int,
     mList: ArrayList<StudentData>
     ) : ArrayAdapter<StudentData>(mContext,resId,mList) {
+
+        val inf = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow = convertView
+
+        if(tempRow == null){
+            tempRow = inf.inflate(R.layout.student_list_item, null)
+        }
+
+        val row = tempRow!!
+
+        return row
+
+    }
+
 }
